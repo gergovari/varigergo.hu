a697e322cfb86ca997a28e98f829be07107fc239
thegergo02 <work@varigergo.xyz>
2023-01-25 09:47:45 +0100

    feat: working popup

--- js/popup.js
+++ js/popup.js
@@ -2,16 +2,24 @@ function setPopupClass(popup, show) {
 	if (popup) {
 		if (show != popup.className.includes("popup-visible")) {
 				popup.className = (show ? popup.className + " " + "popup-visible" : popup.className.replace(" popup-visible", ""));
+		} else {
+			console.log("setPopupclass: Will not add/remove class twice!")
 		}
+	} else {
+		console.log("setPopupclass: Non-existent popup!")
 	}
 }
 function showPopup(name) {
 	if (name) {
 		setPopupClass(document.getElementById(name), true);
+	} else {
+		console.log("showPopup: You need to provide a name!");
 	}
 }
 function hidePopup(name) {
 	if (name) {
 		setPopupClass(document.getElementById(name), false);
+	} else {
+		console.log("showPopup: You need to provide a name!");
 	}
 }
