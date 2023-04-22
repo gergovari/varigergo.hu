ac2419adbe5d23292ac0498d37472b842fdca368
gergovari <gergovari@users.noreply.github.com>
2023-04-22 05:25:33 +0000

    deploy: 0ccc870717699ede6fc8e1d9eb5c897f7a779be2

--- js/gallery.min.54f903c68f0144539b9610ed1b83d9b2da65b995f0f423a97c107e4c366375cac73b8bd0e2c7b779d0586f9acf205c51.js
+++
@@ -1,1 +0,0 @@
-function initGalleries(){const e=new Swiper(".swiper",{slidesPerView:1,spaceBetween:10,pagination:{el:".swiper-pagination"},breakpoints:{640:{slidesPerView:3,spaceBetween:20}}})}window.addEventListener("DOMContentLoaded",function(){initGalleries()}),initGalleries()
